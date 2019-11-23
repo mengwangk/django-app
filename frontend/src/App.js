ad8c8278e972b9f18d2fd104fe708d8306daef54
Koh Meng Wang <mengwangk@gmail.com>
2019-11-23 23:19:34 +0800

    Initial commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import logo from './logo.svg';
+import { Button } from 'antd';
 import './App.css';
 
 function App() {
@@ -19,6 +20,7 @@ function App() {
           Learn React
         </a>
       </header>
+      <Button type="primary">Button</Button>
     </div>
   );
 }
