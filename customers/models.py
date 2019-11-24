006845b0905af904ea568f3b2585f43853a40f62
Koh Meng Wang <mengwangk@gmail.com>
2019-11-24 16:09:56 +0800

    Initial commit

--- customers/models.py
+++ customers/models.py
@@ -1,10 +1,9 @@
 from django.db import models
 
-class User(models.Model):
+class Customer(models.Model):
     first_name = models.CharField("First name", max_length=255)
     last_name = models.CharField("Last name", max_length=255)
     email = models.EmailField()
-    password = models.CharField(max_length=255)
     phone = models.CharField(max_length=20)
     address =  models.TextField(blank=True, null=True)
     description = models.TextField(blank=True, null=True)
